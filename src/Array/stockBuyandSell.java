7f73edaeda8e455dd87b84175d00d032c7f1214c
HemantDigarse <hemantdigarse882@gmail.com>
2026-06-24 19:37:13 +0530

    tcs

---
+++ src/Array/stockBuyandSell.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class stockBuyandSell {
+    public static void main(String[] args) {
+        int prices[] = {7, 10, 1, 3, 6, 9, 2};
+        int maxProfitAns=maxProfit(prices);
+        System.out.println(maxProfitAns);
+    }
+    public static int maxProfit(int[] prices) {
+        int mProfit=0;
+        int ini=prices[0];
+        for(int i=1;i<prices.length;i++){
+            if(ini<prices[i]){
+                mProfit=Math.max(mProfit,prices[i]-ini);
+            }
+            else if(ini>prices[i]){
+                ini=prices[i];
+            }
+        }
+        return mProfit;
+    }
+
+}
